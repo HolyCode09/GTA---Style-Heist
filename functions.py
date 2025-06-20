3be00e6599a540745c4798d9648949c91ff347b2
YarinKogan <Yarinkog2@gmail.com>
2025-06-20 15:16:20 +0300

    finish video editing and add needed sounds for later actions

---
+++ functions.py
@@ -0,0 +1,31 @@
+import pygame
+from pyvidplayer import Video
+
+pygame.mixer.init()
+
+def load_sound(path):
+    return pygame.mixer.Sound(path)
+
+def font_size(size):
+    return pygame.font.Font("gameFont.ttf", size)
+
+def load_img(path, size=None):
+    img = pygame.image.load(path)
+    if size:
+        img = pygame.transform.scale(img, size)
+    return img
+
+def playVid(path, screen, screenWidth, screenHeight):
+    vid = Video(path)
+    vid.set_size((screenWidth,screenHeight))
+    while True:
+        vid.draw(screen, (0,0))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+            if event.type == pygame.QUIT:
+                vid.close()
+                pygame.quit()
+                
+                
