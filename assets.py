5065955b2e7b295dee8bc54a1640234dde3a44b7
Yarin Chen Kogan <yarinkog@gmail.com>
2025-07-10 18:14:22 +0300

    make the screen smaller

--- assets.py
+++ assets.py
@@ -3,8 +3,10 @@ from functions import load_img, load_sound
 
 #images
 gameIcon = load_img("pics/gameIcon.png")
-charactersPic = load_img("pics/characters.png", (1610,910))
-bgPic = load_img("pics/gameEnter.png", (1600, 900))
+charactersPic = load_img("pics/characters.png", (1376,778))
+bgPic = load_img("pics/gameEnter.png", (1366, 768))
+map1 = load_img("pics/maps/map1.png", (1376, 768))
+timeHolder = load_img("pics/screenAssets/timeHolder.png", (300, 100))
 
 #sounds
 opening_theme = load_sound("sounds/OpeningTheme.mp3")
@@ -12,3 +14,7 @@ opening_sound = load_sound("sounds/OpeningRead.mp3")
 characters_sound = load_sound("sounds/Characters.mp3")
 startSound = load_sound("sounds/startSound.mp3")
 video_bg_music = load_sound("sounds/videoMusic.mp3")
+missionMusic = load_sound("sounds/missionMusic.mp3")
+clockTick = load_sound("sounds/clockTick.mp3")
+carStops = load_sound("sounds/carStops.mp3")
+
--- screens.py
+++ screens.py
@@ -9,11 +9,16 @@ from assets import (
     opening_sound,
     characters_sound,
     startSound,
-    video_bg_music
+    video_bg_music,
+    missionMusic,
+    map1,
+    clockTick,
+    carStops, 
+    timeHolder
 )
 from functions import font_size, playVid
 
-screenWidth, screenHeight = 1600,900
+screenWidth, screenHeight = 1366,768
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("GTA - Style Heist")
 pygame.display.set_icon(gameIcon)
@@ -142,6 +147,8 @@ def CharactersScreen():
 
 def preGameVideo():
     global video_bg_music
+    pygame.mouse.set_visible(False)
+
     pygame.time.delay(1000)
     video_bg_music.set_volume(0)
     video_bg_music.play(-1)
@@ -151,4 +158,76 @@ def preGameVideo():
     pygame.time.delay(1000)
     playVid("video/gameVideo.mp4", screen, screenWidth, screenHeight)
     video_bg_music.stop()
+    gameStart()
     return
+
+
+def gameStart():
+    missionMusic.set_volume(1)
+    missionMusic.play(-1)
+
+    clock = pygame.time.Clock()
+
+    minuteChange = 0
+
+    screen.fill((0, 0, 0))
+
+    pygame.time.delay(1000)
+
+    for alpha in range(0, 256, 1):
+        startText = font_size(180).render("You have until 9PM", True, (alpha, alpha, alpha))
+        screen.blit(startText, (screenWidth // 2 - startText.get_width() // 2, screenHeight // 2 - startText.get_height() // 2))
+        pygame.display.flip()
+        pygame.time.delay(10)
+
+    pygame.time.delay(4000)
+
+    for alpha in range(0, 256, 10):
+        fade_surface = pygame.Surface((screenWidth, screenHeight))
+        fade_surface.fill((0, 0, 0))
+        fade_surface.set_alpha(alpha)
+        screen.blit(fade_surface, (0, 0))
+        pygame.display.flip()
+        pygame.time.delay(20)
+
+    pygame.time.delay(1000)
+
+    carStops.set_volume(0.8)
+    carStops.play()
+
+    clock_text = font_size(70).render("8:00PM", True, (255, 255, 255))
+    for alpha in range(255, -1, -1):
+        fade_surface = pygame.Surface((screenWidth, screenHeight))
+        fade_surface.fill((0, 0, 0))
+        fade_surface.set_alpha(alpha)
+        screen.blit(map1, (-10, 0))
+        screen.blit(timeHolder, (screenWidth // 2 - timeHolder.get_width() // 2, -20))
+        screen.blit(clock_text, (screenWidth // 2 - clock_text.get_width() // 2, 0))
+        screen.blit(fade_surface, (0, 0))
+        pygame.display.flip()
+        pygame.time.delay(10)
+
+    start_ticks = pygame.time.get_ticks()
+    while True:
+        screen.blit(map1, (-10, 0))
+
+        current_ticks = pygame.time.get_ticks()
+        elapsed_time = current_ticks - start_ticks
+        in_game_minutes = elapsed_time // 20000
+        in_game_hour = 8 + (in_game_minutes // 60)
+        in_game_minutes = in_game_minutes % 60
+
+        clock_text = font_size(70).render(f"{in_game_hour}:{in_game_minutes:02}PM", True, (255, 255, 255))
+        screen.blit(timeHolder, (screenWidth // 2 - timeHolder.get_width() // 2, -20))
+        screen.blit(clock_text, (screenWidth // 2 - clock_text.get_width() // 2, 0))
+        if minuteChange != in_game_minutes:
+            clockTick.play()
+            minuteChange = in_game_minutes
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.flip()
+        clock.tick(60)
+
